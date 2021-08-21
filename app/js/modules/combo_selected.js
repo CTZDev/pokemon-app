b381257c4ac9cb36d91846f34e8d54b110427041
CTZDev <carlosteranzavaleta@hotmail.com>
2021-08-21 10:57:48 -0500

    add cmbBox - design and functionality

--- app/js/modules/combo_selected.js
+++ app/js/modules/combo_selected.js
@@ -1,20 +1,23 @@
 const d = document;
 
 const selectedMenu = (e) => {
-  const $selectWrapper = d.querySelector(".select-wrapper");
-  const $customOption = d.querySelectorAll(".custom-option");
   const wrapper = ".select-wrapper";
 
   d.addEventListener("click", (e) => {
     if (e.target.matches(`${wrapper} *`)) {
-      d.querySelector(".select").classList.toggle("open");
+      const $selected = e.target.closest(".select");
+      $selected.classList.toggle("open");
     }
 
     if (e.target.matches(".custom-option")) {
       const $option = e.target;
-      [...$customOption].map((option) => option.classList.remove("selected"));
+      const $listOptions = $option.parentElement.children;
+      const $parentOption = $option.closest(".select");
+      const $customSelectText = $parentOption.querySelector(".select-trigger > span");
+
+      [...$listOptions].map((option) => option.classList.remove("selected"));
       $option.classList.add("selected");
-      $option.closest(".select").querySelector(".select__trigger span").textContent = $option.textContent;
+      $customSelectText.textContent = $option.textContent;
     }
 
     if (!e.target.matches(`${wrapper} *`)) {
